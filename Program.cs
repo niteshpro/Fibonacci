868f9071905103a89e2ce196dc91e265bb453a8d
Nitesh Sharma <niteshsharma9199@gmail.com>
2022-09-04 17:58:43 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Fibonacci
+{
+    public static class Program
+    {
+        static void Main()
+        {
+            int n1=0, n2=1,n3,size=15;
+            Console.Write(n1 +" "+ n2+ " ");
+            for(int i = 2; i < size; i++)
+            {
+                n3= n1+n2;
+                Console.Write(n3+" ");
+                n1 = n2;
+                n2= n3;
+            }
+
+
+        }
+    }
+}
